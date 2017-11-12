68c5ca2cf5ee06285ab261a2c76791912c896178
mahpa94 <mahpatlv94@gmail.com>
2017-11-12 13:38:00 -0500

    MVC

---
+++ AplicacionWeb/Controllers/API/ProductosController.cs
@@ -0,0 +1,28 @@
+﻿using GestionDeTienda;
+using InterfacesInventario.Productos;
+using InterfacesInventario.Productos.Peticiones;
+using InterfacesInventario.Productos.Respuestas;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Web.Controllers.API
+{
+    public class ProductosController : ApiController
+    {
+        private IGestorDeProductos _gestorDeProductos = new GestorDeProductos();
+        public List<ProductoRegistrado> Get()
+        {
+            return _gestorDeProductos.ListarTodosLosProductos();
+        }
+
+        // POST: api/Productos
+        public void Post(NuevoProducto nuevoProducto)
+        {
+            _gestorDeProductos.CrearProducto(nuevoProducto);
+        }
+    }
+}
---
+++ AplicacionWeb/Controllers/ProductosController.cs
@@ -0,0 +1,26 @@
+﻿using GestionDeTienda;
+using InterfacesInventario.Productos;
+using InterfacesInventario.Productos.Respuestas;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web.Controllers
+{
+    public class ProductosController : Controller
+    {
+        private IGestorDeProductos _gestorDeProductos = new GestorDeProductos();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult _TablaDeProductos()
+        {
+            List<ProductoRegistrado> listaDeProductos = _gestorDeProductos.ListarTodosLosProductos();
+            return View(listaDeProductos);
+        }
+    }
+}
